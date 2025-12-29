1262549e35e1e47f0197d5805538b1cb40ae3970
MahmoudAshrafE <mamo056998@gmail.com>
2025-12-29 14:58:14 +0200

    fix: explicit fallback to DIRECT_URL in runtime prisma client to prevent build errors

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!
+  connectionString: process.env.DATABASE_URL || process.env.DIRECT_URL!
 });
 
 // Pass the adapter in options
